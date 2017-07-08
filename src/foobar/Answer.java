d32091af720c94fdc139163c9fa72185638030a1
JamesOsborne <JamesOsborne@10.0.0.41>
2017-07-08 14:57:18 -0400

    Fixed stuff

--- src/foobar/Answer.java
+++ src/foobar/Answer.java
@@ -51,20 +51,20 @@ public class Answer {
                 }
             }
             
-            brokenWallLocations[wall.getY()][wall.getX()] = new Location(0, wall.getY(), wall.getX(), -1);
+            brokenWallLocations[wall.y][wall.x] = new Location(0, wall.y, wall.x, -1);
             
             while (!pathTaken.isEmpty()) {
                 Location currLocation = pathTaken.remove();
 
-                if (currLocation.getType() == 3) {
+                if (currLocation.type == 3) {
                     found = true;
                     endingLocation = currLocation;
                     break;
                 }
 
-                int currDist = currLocation.getDistance();
-                int x = currLocation.getX();
-                int y = currLocation.getY();
+                int currDist = currLocation.distance;
+                int x = currLocation.x;
+                int y = currLocation.y;
 
                 if (x > 0) {
                     setAdjacentDistances(brokenWallLocations[y][x - 1], currDist);
@@ -73,7 +73,12 @@ public class Answer {
                     setAdjacentDistances(brokenWallLocations[y][x + 1], currDist);
                 }
                 if (y > 0) {
-                    setAdjacentDistances(brokenWallLocations[y - 1][x], currDist);
+                    try {
+                        setAdjacentDistances(brokenWallLocations[y - 1][x], currDist);
+                    }
+                    catch(Exception e) {
+                        throw e;
+                    }
                 }
                 if (y < rows - 1) {
                     setAdjacentDistances(brokenWallLocations[y + 1][x], currDist);
@@ -81,10 +86,10 @@ public class Answer {
             }
 
             if (found) {
-                System.out.println("Distance traveled = " + endingLocation.getDistance());
+                System.out.println("Distance traveled = " + endingLocation.distance);
                 
-                if (result > endingLocation.getDistance()) {
-                    result = endingLocation.getDistance();
+                if (result > endingLocation.distance) {
+                    result = endingLocation.distance;
                     System.out.println("Faster route: " + result);
                 }
             }
@@ -94,7 +99,7 @@ public class Answer {
             
             for (int i = 0; i < rows; ++i) {
                 for (int j = 0; j < columns; ++j) {
-                    locations[i][j].setDist(-1);
+                    locations[i][j].distance = -1;
                 }
             }
         }
@@ -103,110 +108,48 @@ public class Answer {
     }
     
     private static void setAdjacentDistances(Location location, int adjDist) {
-        if (location.getDistance() == -1 && location.getType() != 1) {
-            location.setDist(adjDist + 1);
-            
+        if (location.distance == -1 && location.type != 1) {
+            location.distance = adjDist + 1;            
             pathTaken.add(location);
         }
     }
     
-    public static class Coord {
-        private int x;
-        private int y;
-
-        public Coord() {
-            this(-1, -1);
-        }
-
-        public Coord(int numX, int numY) {
-            x = numX;
-            y = numY;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-
-        public void setX(int num) {
-            x = num;
-        }
-
-        public void setY(int num) {
-            y = num;
-        }
-    }
-    
     public static class Location {
-        private int type;
-        private Coord coord;
-        private int distance;
+        public int type;
+        public int x;
+        public int y;
+        public int distance;
     
         public Location() { }
         public Location(Location l) {
-            type = l.getType();
-            coord = new Coord(l.getX(), l.getY());
-            distance = l.getDistance();
+            type = l.type;
+            x = l.x;
+            y = l.y;
+            distance = l.distance;
         }
-    
-        public Location(int t, int x, int y, int d) {
+        public Location(int t, int i, int j, int dist) {
             type = t;
-            coord = new Coord(x, y);
-            distance = d;
-        }
-    
-        public void printCoord() {
-            System.out.print(coord.getY() + " " + coord.getX());
-        }
-
-        public int getType() {
-            return type;
-        }
-
-        public Coord getCoord() {
-            return coord;
-        }
-
-        public int getX() {
-            return coord.getX();
-        }
-
-        public int getY() {
-            return coord.getY();
-        }
-
-        public int getDistance() {
-            return distance;
-        }
-
-        public void setType(int t) {
-            type = t;
-        }
-
-        public void setCoord(Coord c) {
-            coord = c;
-        }
-
-        public void setX(int x) {
-            coord.setX(x);
-        }
-
-        public void setY(int y) {
-            coord.setY(y);
-        }
-
-        public void setDist(int d) {
-            distance = d;
+            x = i;
+            y = j;
+            distance = dist;
         }
     }
     
     public static void main(String[] args) {
-        int[][] maze = {{0, 1, 0},
-                        {1, 0, 1},
-                        {0, 0, 0}};
+        int[][] maze = {{0, 0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0, 0},
+                        {0, 1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0, 0},
+                        {1, 1, 1, 1, 1, 0},
+                        {0, 0, 0, 0, 0, 0},
+                        {0, 1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0, 0},
+                        {1, 1, 1, 1, 1, 0},
+                        {0, 0, 0, 0, 0, 0},
+                        {0, 1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0, 0},
+                        {1, 1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0, 0}};
         System.out.println("Ending distance: " + answer(maze));
     }
 }
